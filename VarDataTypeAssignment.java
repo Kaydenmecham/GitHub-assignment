9f36cae71205c2b69a346eb1ce1833d88c489f78
Kaydenmecham <68206920+Kaydenmecham@users.noreply.github.com>
2020-07-12 21:47:24 -0600

    Add files via upload

---
+++ VarDataTypeAssignment.java
@@ -0,0 +1,22 @@
+// Kayden Mecham
+public class VarDataTypeAssignment {
+
+	public static void main(String[] args) {
+System.out.print("=========================\n");
+System.out.println("Student Report");
+System.out.print("=========================\n");
+String StudentFirstName = "Bob Smith";
+System.out.println("Student Name:\t" + StudentFirstName);
+byte Age = 20; 
+double GPA = 3.52;
+System.out.println("Age: " + Age + "\t\t" + "GPA: " + GPA);
+String Major = "Biology";
+System.out.println("Major: " + Major);
+String Class = "Sophmore";
+System.out.println("Class: " + Class);
+System.out.print("=========================");
+
+
+	}
+
+}
